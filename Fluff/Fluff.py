063f047b16a7aa400b20c9bbcc59ddc00ae72da9
Cis112233 <christopherpeart@hotmail.com>
2017-02-13 19:44:57 -0500

    Added my start
    Just started

---
+++ Fluff/Fluff.py
@@ -0,0 +1,22 @@
+stack = []
+results
+def pushToStack(a):#Adds to stack
+	stack.append(a)
+
+def getFromStack():
+	return (stack[0])
+
+def getSStack(a):
+	return (stack[a])
+
+def oneArgs(a):#not added
+	return 0
+
+def twoArgs(a,b):
+	return 0
+
+def toThe(a,b):
+	return a^b
+
+def multi(a,b):
+	return a*b
