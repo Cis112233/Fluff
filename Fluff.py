d359f2599e254e78f01740851e8ebd469447224e
Cis112233 <christopherpeart@hotmail.com>
2017-02-27 19:08:14 -0500

    Started some work on eval

--- Fluff.py
+++ Fluff.py
@@ -1,5 +1,32 @@
 stack = []
+input = 
 results
+oneArgDict = {
+"♂":"",#11
+"♀":"",#12
+"♪":"",#13...
+"♫":"",
+"☼":"",
+"►":"",
+"◄":"",
+"↕":"",
+"‼":"",
+"§":"",
+"▬":"",
+"↨":"",
+"↑":"",
+}
+twoArgDict = {
+"^":"power",
+"%":"mods",
+"*":"multi",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+"":"",
+}
 def pushToStack(a):#Adds to stack
 	stack.append(a)
 
@@ -12,11 +39,15 @@ def getSStack(a):
 def oneArgs(a):#not added
 	return 0
 
-def twoArgs(a,b):
-	return 0
-
-def toThe(a,b):
+def twoArgs(a,b,c):
+	eval(twoArgDict[a]+"("+b+c+")")
+def power(a,b):
 	return a^b
 
 def multi(a,b):
 	return a*b
+def mods(a,b):
+    return mod(a,b)
+
+def runRegEx():
+    return 0
